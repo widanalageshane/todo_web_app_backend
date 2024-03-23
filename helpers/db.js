381a684ad9c10487876de67e761cdd568a91eabe
Shane Widanalage <t3wish00@students.oamk.fi>
2024-03-23 17:59:24 +0200

    env.vault added

--- helpers/db.js
+++ helpers/db.js
@@ -19,6 +19,7 @@ const openDb = () => {
         database: process.env.DB_NAME,      
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
+        ssl: process.env.SSL
     });
     return pool;
 }
